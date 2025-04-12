669134d5fcd26ae0928db6c84314fe4f8cbea7c5
rockmel03 <kamalswag2@gmail.com>
2025-04-12 22:55:24 +0530

    Add cookie-parser middleware for enhanced request handling and implement CORS configuration with whitelisted origins for improved security.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,28 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import errorHandler from "./utils/errorHandler.js";
+import corsOptions from "./config/corsOptions.js";
 
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// middlewares
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// routes
 app.get("/", (req, res) => {
   res.status(200).send("hello world!");
 });
+
+// default error handler
+app.use(errorHandler);
 
 export default app;
--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "e-commerce";
+export const WHITELISTED_ORIGINS = ["http://localhost:5173"];
